17d8cb5e6b09e23aa4d1fbb3b065189d67964f45
Janet侒然 <janet168@126.com>
2021-01-27 18:44:45 +0800

    修改端口

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+   host: env('HOST', '0.0.0.0'),
+  // host: env('HOST', 'http://47.99.58.128'),
+  port: env.int('PORT', 1339),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '210c18d7e362d837212d64561d4bdcf6'),
