3ae43b1d58d26cd1462c099d7b2f3b531504e50d
Janet侒然 <janet168@126.com>
2021-02-03 15:08:55 +0800

    xg

--- config/server.js
+++ config/server.js
@@ -1,17 +1,12 @@
 const forgotPasswordTemplate =require('../config/email-templates/forgot-password.js')
 
 module.exports = ({ env }) => ({
-  //  host: env('HOST', '0.0.0.0'),
-  host: env('HOST', 'http://47.99.58.128'),
+   host: env('HOST', '0.0.0.0'),
+  // host: env('HOST', 'http://47.99.58.128'),
   port: env.int('PORT', 1339),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '210c18d7e362d837212d64561d4bdcf6'),
-    },
-    forgotPassword: {
-      from: '327243342@qq.com',
-      replyTo: '327243342@qq.com',
-      emailTemplate: forgotPasswordTemplate,
-    },
+    }
   },
 });
