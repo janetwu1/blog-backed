9d608cc8591879c5e3052f9d6c741eff531bd291
Janet侒然 <janet168@126.com>
2021-02-03 16:02:32 +0800

    dz

--- config/database.js
+++ config/database.js
@@ -20,8 +20,8 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        //  host: env('DATABASE_HOST', 'localhost'),
-        host: env('DATABASE_HOST', 'http://47.99.58.128'),
+        host: env('DATABASE_HOST', 'localhost'),
+        // host: env('DATABASE_HOST', 'http://47.99.58.128'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'blog'),
