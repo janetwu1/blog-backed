fef3f5fa773f77d964e03eb1e2d6688bdcead32d
janetwu1 <45478475+janetwu1@users.noreply.github.com>
2022-12-12 10:05:46 +0800

    修改 args

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name: 'blog-backed',
     script: '/usr/local/bin/npm',
+    args: 'start',
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
